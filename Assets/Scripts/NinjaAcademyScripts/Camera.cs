6edb6204433acab8b714610b3f5bb0a85545ac6c
DJuanOrdonez <sebastianordonez@unicauca.edu.co>
2022-03-10 16:16:15 -0500

    Fix

--- Assets/Scripts/NinjaAcademyScripts/Camera.cs
+++ Assets/Scripts/NinjaAcademyScripts/Camera.cs
@@ -7,7 +7,7 @@ public class Camera : MonoBehaviour
 
     public Transform target;
 
-    public Transform farBackground, middleBackground;
+    public Transform middleBackground, farBackground;
 
     private float lastxPosition;
 
@@ -22,15 +22,15 @@ public class Camera : MonoBehaviour
     {
         transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
 
-        if (target.position.x < 0f ) transform.position = new Vector3(0f, -2.5f, transform.position.z);
+        if (target.position.x < 0f ) transform.position = new Vector3(0f, -2.2f, transform.position.z);
 
         if (target.position.y > -2.5 && target.position.x > 0f) transform.position = new Vector3(target.position.x, target.position.y, transform.position.z); ;
 
 
         float amountToMoveX = transform.position.x - lastxPosition;
 
-        farBackground.position = farBackground.position + new Vector3(amountToMoveX * -0.002f, 0f, 0f);
-        middleBackground.position += new Vector3(amountToMoveX * 0.6f,0f, 0f);
+        farBackground.position = farBackground.position + new Vector3(amountToMoveX * -0.0012f, 0f, 0f);
+        middleBackground.position += new Vector3(amountToMoveX * 0.4f,0f, 0f);
 
         lastxPosition = transform.position.x;
         
